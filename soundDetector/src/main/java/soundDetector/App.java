fa733e6a1e339da48f1f916ada8ad58920f0cb1b
Michal Brka <michal.brka@notix.cz>
2018-12-04 12:40:30 +0100

    parallel program

--- soundDetector/src/main/java/soundDetector/App.java
+++ soundDetector/src/main/java/soundDetector/App.java
@@ -3,6 +3,14 @@ package soundDetector;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.openimaj.audio.AudioPlayer;
 import org.openimaj.audio.SampleChunk;
 import org.openimaj.audio.analysis.FourierTransform;
@@ -30,8 +38,8 @@ public class App {
      * @trimDuration dlzka skratenie skladby
      * @descriptorsPerSong pocet usekov, pre ktore sa pocitaju descriptory
      */
-    private static final int trimDuration = 2;
-    private static final int descriptorsPerSong = 20;
+    private static final int trimDuration = 5;
+    private static final int descriptorsPerSong = 5;
     private static Model model;
     private static FileManager fileManager;
     private static SongManager songManager;
@@ -45,24 +53,26 @@ public class App {
         songManager = new SongManager(model);
         des = new DescriptorCalculator(model);
         
+        
+        
         //upload all cluster songs to model
-        //uploadClusterSongs();
+        uploadClusterSongs();
         
         //Jedna varianta je urobit uploadClusterSongs() a nasledne si vysledok ulozit cez fileManager.exportToFile(Path to file)
         //Druha varianta je nacitanie descriptorov cez fileManager.importFromFile(Path to file) 
         
-        //fileManager.exportToFile("C:\\Own_Projects\\MI-VMW\\tests\\export.json");
-        fileManager.importFromFile("C:\\Own_Projects\\MI-VMW\\tests\\export.json");
+        fileManager.exportToFile("C:\\Own_Projects\\MI-VMW\\tests\\export.json");
+        //fileManager.importFromFile("C:\\Own_Projects\\MI-VMW\\tests\\export.json");
         uploadWorkingSong("C:\\Own_Projects\\MI-VMW\\tests\\Sunshine Original Soundtrack - Surface of the Sun (Early promo version) [HD].mp4.mp3");
         System.out.println("OneToMany:");
         
-        //pocitanie 1:1 s vahami na descriptoroch
+        //pocitanie 1:n s vahami na descriptoroch
         clusterManager.computeClusteringResultOneToMany().printResults();
         System.out.println("");
         System.out.println("");
         System.out.println("OneToOne:");
         
-        //pocitanie 1:n s vahami na descriptoroch
+        //pocitanie 1:1 s vahami na descriptoroch
         clusterManager.computeClusteringResultOneToOne().printResults();
         
     }
@@ -80,6 +90,10 @@ public class App {
      * upload all clusters songs from different genres, trim them and compute Descriptors
      */
     private static void uploadClusterSongs(){
+        //ExecutorService executor = Executors.newFixedThreadPool(8);
+        ThreadPoolExecutor executorPool = new ThreadPoolExecutor(16, 16, 10, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(getNumSongs()+1));
+        
+        final CountDownLatch latch = new CountDownLatch(getNumSongs());
         File[] genres = new File("../genres/").listFiles();
         for (File genre : genres) {
             File[] songs = genre.listFiles(new FilenameFilter() {
@@ -93,12 +107,39 @@ public class App {
             }
             for (File song : songs) {
                 System.out.println("Song name: "+song.getName()+", song genre: "+song.getParentFile().getName());
-                Song songTmp = new Song(song);
+                final File s = song;
+                final File g = genre;
+                executorPool.submit(new Runnable(){ public void run() { 
+  Song songTmp = new Song(s);
                 songManager.trimSong(songTmp, trimDuration);
                 des.computeDescriptors(songTmp, descriptorsPerSong);
                 songTmp.setAudio(null);
-                model.getClusters().get(genre.getName()).getSongs().add(songTmp);                
+                model.getClusters().get(g.getName()).getSongs().add(songTmp); 
+                latch.countDown();
+                    System.out.println("latch:"+latch.getCount());
+}});            
             }
         }
+        try {
+            latch.await();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        executorPool.shutdown();
+    }
+
+    private static int getNumSongs() {
+        int n = 0;
+        File[] genres = new File("../genres/").listFiles();
+        for (File genre : genres) {
+            File[] songs = genre.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.endsWith(".mp3");
+                }
+            });
+            n+=songs.length;
+        }
+        return n;
     }
 }
